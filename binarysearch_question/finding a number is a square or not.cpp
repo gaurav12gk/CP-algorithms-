0e15d67cea50a2f0f582fcaee6c85731e6c796de
Gaurav Kandpal <kandpal862@gmail.com>
2020-07-30 18:15:11 +0530

    bsagain

---
+++ binarysearch_question/finding a number is a square or not.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define line "\n"
+#define loop(i,a,n) for(ll i=a;i<n;i++)
+#define ll long long int
+#define lb long double
+#define ull unsigned long long int
+#define pb push_back
+#define vi vector < int >
+# define vs vector < string >
+# define vb vector < bool >
+#define vll vector<ll>
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+# define pld pair < ld , ld >
+# define pDD pair < D , D >
+#define vpii vector<pair<int,int>>
+#define vpsi vector<pair<string,int>>
+#define mii map<int ,int>
+#define mis map<int,string>
+#define all(v) v.begin(),v.end()
+
+
+# define fast ios_base::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0);
+using namespace std;
+
+void solve()
+{ 
+int n;
+cin>>n;
+int low=1;
+int high=n;
+while(high>=low)
+{
+    int mid=(high+low)/2;
+    if(mid*mid==n){cout<<mid;return;}
+    else if(mid*mid>n)high=mid-1;
+    else low=mid+1;
+}
+cout<<"notfound";
+}
+
+
+
+int main()
+{fast
+#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("output.txt","w",stdout);
+ #endif
+ll t;
+cin>>t;
+while(t--)
+{
+solve();
+}
+
+}
+
