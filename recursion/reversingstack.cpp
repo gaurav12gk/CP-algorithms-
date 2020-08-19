918db5bfea3ae99ab50c525f663a433e8679b383
Gaurav Kandpal <kandpal862@gmail.com>
2020-08-19 14:10:22 +0530

    initial

---
+++ recursion/reversingstack.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define line "\n"
+#define loop(i,a,n) for(ll i=a;i<n;i++)
+#define ll long long int
+#define lb long double
+#define ull unsigned long long int
+#define pb push_back
+#define vi vector < int >
+# define vs vector < string >
+# define vb vector < bool >
+#define vll vector<ll>
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+# define pld pair < ld , ld >
+# define pDD pair < D , D >
+#define vpii vector<pair<int,int>>
+#define vpsi vector<pair<string,int>>
+#define mii map<int ,int>
+#define mis map<int,string>
+#define all(v) v.begin(),v.end()
+
+
+# define fast ios_base::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0);
+using namespace std;
+stack<int> r;
+void insert(stack<int>&s,int n)
+{
+    if(s.size()==n)return;
+    else 
+    {
+        s.push(r.top());
+    }
+    return;
+}
+void reversee(stack<int>&s)
+{
+if(s.size()==0)
+{
+    insert(s,r.size());
+return;
+}
+else{
+int val=s.top();
+s.pop();
+r.push(val);
+reversee(s);
+   }
+
+}
+void solve()
+
+{ 
+
+   int n;
+    cin>>n;
+    stack<int> s;
+    loop(i,0,n)
+    {int a;
+    cin>>a;s.push(a);
+        }   
+      //  int middle = n/2+bool(n/2);
+    reversee(s);
+while(!r.empty())
+       {
+           cout<<r.top()<<line;
+           r.pop();
+       }
+}
+
+
+
+int main()
+{fast
+#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("output.txt","w",stdout);
+ #endif
+ll t;
+cin>>t;
+while(t--)
+{
+solve();
+}
+
+}
+
