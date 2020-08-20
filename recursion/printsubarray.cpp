2542a6a3b70cde84c57cadf240c7d013734af400
Gaurav Kandpal <kandpal862@gmail.com>
2020-08-20 13:38:28 +0530

    intial

---
+++ recursion/printsubarray.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define line "\n"
+#define loop(i,a,n) for(ll i=a;i<n;i++)
+#define ll long long int
+#define lb long double
+#define ull unsigned long long int
+#define pb push_back
+#define vi vector < int >
+# define vs vector < string >
+# define vb vector < bool >
+#define vll vector<ll>
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+# define pld pair < ld , ld >
+# define pDD pair < D , D >
+#define vpii vector<pair<int,int>>
+#define vpsi vector<pair<string,int>>
+#define mii map<int ,int>
+#define mis map<int,string>
+#define all(v) v.begin(),v.end()
+
+
+# define fast ios_base::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0);
+using namespace std;
+int count=0;
+void sub(string ip,string op)
+{
+    if(ip.size()==0){
+        cout<<op<<" ";
+        ::count++;
+        return;
+    }
+    string op1=op;
+    string op2=op;
+    op2.push_back(ip[0]);
+    ip.erase(ip.begin()+0);
+    sub(ip,op2);
+    sub(ip,op1);
+}
+void solve()
+{ 
+string a;
+cin>>a;
+string op="";
+sub(a,op);
+cout<<line<<::count;
+}
+
+
+
+int main()
+{fast
+#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("output.txt","w",stdout);
+ #endif
+ll t;
+cin>>t;
+while(t--)
+{
+solve();
+}
+
+}
+
