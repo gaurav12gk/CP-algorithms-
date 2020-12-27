03eab987233379aa639381116863b4bd0933161b
Gaurav Kandpal <kandpal862@gmail.com>
2020-12-27 17:02:05 +0530

    initial

---
+++ String_matching_algo/Sliding_Window_matching.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define line "\n"
+#define loop(i,a,n) for(ll i=a;i<n;i++)
+#define ll long long int
+#define lb long double
+#define ull unsigned long long int
+#define pb push_back
+#define vi vector < int >
+# define vs vector < string >
+# define vb vector < bool >
+#define vll vector<ll>
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+# define pld pair < ld , ld >
+# define pDD pair < D , D >
+#define vpii vector<pair<int,int>>
+#define vpsi vector<pair<string,int>>
+#define mii map<int ,int>
+#define mis map<int,string>
+#define all(v) v.begin(),v.end()
+# define fast ios_base::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0);
+using namespace std;
+const int row[]={0,0,1,1,-1,1,-1,-1};
+const int col[]={1,-1,-1,1,1,0,0,-1};
+string a;
+
+string s;
+int aa,ss;
+
+ bool match(int l,int r)
+ {
+     for(int i=0;i<ss;i++)
+     if(a[i+l]!=s[i])return false;
+      return true;
+ }
+
+
+
+
+void solve()
+{ 
+
+cin>>a;
+cin>>s;
+ aa=a.size();
+ ss=s.size();
+for(int l=0,r=ss-1;r<aa;l++,r++)
+if(match(l,r))cout<<l<<" ";
+
+
+}
+
+int main()
+{fast
+#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("output.txt","w",stdout);
+ #endif
+ll t;
+cin>>t;
+while(t--)
+{
+solve();
+}
+
+}
+
